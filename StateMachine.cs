7b4d9f6306b870d8f962c6293aa0b50c524cf379
Syy <41816113+Syy9@users.noreply.github.com>
2019-05-18 19:12:11 +0900

    Update StateMachine.cs

--- StateMachine.cs
+++ StateMachine.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-namespace StatePattern
+namespace Syy.Logics
 {
     public abstract class StateMachine<TManager, TOwner> where TManager : StateMachine<TManager, TOwner>
     {
